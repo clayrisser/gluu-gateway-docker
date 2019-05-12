3b21c45a1fb0692ca62964f7d152234043b030b8
Jam Risser <jam@codejam.ninja>
2019-05-12 02:01:14 -0500

    Added database migration

--- konga/latest/entrypoint.sh
+++ konga/latest/entrypoint.sh
@@ -11,5 +11,6 @@ fi
 
 sleep 5
 psql -h "$DB_HOST" -U "$DB_USER" -c "CREATE DATABASE $DB_DATABASE;"
+psql -h "$DB_HOST" -U "$DB_USER" $DB_DATABASE < /opt/app/konga_db.sql
 
 exec /bin/sh /opt/app/start.sh "$@"
