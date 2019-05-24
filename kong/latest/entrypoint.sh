91d47af4d8b2adfe7773a8d957d07958502ed177
Jam Risser <jam@codejam.ninja>
2019-05-23 22:49:49 -0500

    Updated database scripts

--- kong/latest/entrypoint.sh
+++ kong/latest/entrypoint.sh
@@ -12,7 +12,11 @@ fi
 confd -onetime -backend env
 
 sleep 5
-psql -h "$KONG_PG_HOST" -U "$KONG_PG_USER" -c "CREATE DATABASE $KONG_PG_DATABASE;"
-/docker-entrypoint.sh kong migrations up
+if [ ! $(psql -h "$KONG_PG_HOST" -U "$KONG_PG_USER" -l | cut -d \| -f 1 | grep -qw $KONG_PG_DATABASE) ]; then
+  psql -h "$KONG_PG_HOST" -U "$KONG_PG_USER" -c "CREATE DATABASE $KONG_PG_DATABASE;"
+fi
+if [ ! $(psql -h "$KONG_PG_HOST" -U "$KONG_PG_USER" -c "\c $KONG_PG_DATABASE" -c "\dt" | cut -d \| -f 2 | grep -qw profiles) ]; then
+  /docker-entrypoint.sh kong migrations up
+fi
 
 exec /docker-entrypoint.sh "$@"
--- konga/latest/entrypoint.sh
+++ konga/latest/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 sleep 5
-psql -h "$DB_HOST" -U "$DB_USER" -c "CREATE DATABASE $DB_DATABASE;"
-psql -h "$DB_HOST" -U "$DB_USER" $DB_DATABASE < /opt/app/konga_db.sql
+if [ ! $(psql -h "$DB_HOST" -U "$DB_USER" -l | cut -d \| -f 1 | grep -qw $DB_DATABASE) ]; then
+  psql -h "$DB_HOST" -U "$DB_USER" -c "CREATE DATABASE $DB_DATABASE;"
+fi
+if [ ! $(psql -h "$DB_HOST" -U "$DB_USER" -c "\c $DB_DATABASE" -c "\dt" | cut -d \| -f 2 | grep -qw konga_api_health_checks) ]; then
+  psql -h "$DB_HOST" -U "$DB_USER" $DB_DATABASE < /opt/app/konga_db.sql
+fi
 
 exec node /opt/app/start.js "$@"
