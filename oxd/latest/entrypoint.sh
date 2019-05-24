76644c2f7d2fe6023a79904ec9f4e9c837d7af1a
Jam Risser <jam@codejam.ninja>
2019-05-23 21:11:54 -0500

    Finished base oxd server

--- oxd/latest/entrypoint.sh
+++ oxd/latest/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 confd -onetime -backend env
-/etc/init.d/oxd-server start
 
-exec tail -f /dev/null
+exec java -Djava.net.preferIPv4Stack=true -cp bcprov-jdk15on-1.54.jar:oxd-server-4.0-SNAPSHOT.jar org.xdi.oxd.server.OxdServerApplication server /opt/oxd-server/oxd-server.yml
